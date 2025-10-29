3da70219f83a08c4f3a0dde8836f7bfd39898ae5
Williams B. Gomes <williamsgomes45@gmail.com>
2025-10-29 19:28:01 -0300

    Criando o componente Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import { Header } from "@/components/header";
+import { Header } from "@/components/header/header";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
