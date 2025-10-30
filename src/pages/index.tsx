d8be7c971ef63e2f19e55e13edf570dbb0f9d360
Williams B. Gomes <williamsgomes45@gmail.com>
2025-10-30 17:21:52 -0300

    Criando seção Feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { FeatureSection } from "@/components/feature-section";
 import { HeroSection } from "@/components/hero-section";
 
 export default function Home() {
 	return (
 		<article className="flex flex-col">
 			<HeroSection />
+			<FeatureSection />
 		</article>
 	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,6 +63,9 @@ export default {
         "body-sm": ["14px", { lineHeight: "150%", fontWeight: "400" }],
         "body-xs": ["12px", { lineHeight: "150%", fontWeight: "400" }],
 
+        // Body Tag
+        "body-tag": ["12px", { lineHeight: "100%", fontWeight: "400" }],
+
         // Action - Inter
         "action-md": ["16px", { lineHeight: "normal", fontWeight: "500" }],
         "action-sm": ["14px", { lineHeight: "normal", fontWeight: "500" }],
