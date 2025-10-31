5523baf9651255b547eff403d9a1f453a76c9184
Williams B. Gomes <williamsgomes45@gmail.com>
2025-10-30 21:33:41 -0300

    Criando seção Support

---
+++ src/components/support-section/support-section.tsx
@@ -0,0 +1,95 @@
+import { HeartHandshake, PaintbrushVertical, Store } from "lucide-react";
+import { PT_Sans_Caption } from "next/font/google";
+
+const ptSansCaption = PT_Sans_Caption({
+	subsets: ["latin"],
+	weight: "700",
+});
+
+export function SupportSection() {
+	return (
+		<section className="pb-8 md:py-10 bg-gray-700">
+			<div className="container flex flex-col items-center gap-12">
+				<h2
+					className={`
+            ${ptSansCaption.className}
+            text-balance text-center text-heading-lg text-gray-100
+          `}
+				>
+					Sua loja de afiliados, simples, do jeito que deveria ser
+				</h2>
+
+				<div className="grid gap-6 md:grid-cols-3">
+					{/* Primeiro Card */}
+					<div
+						className={`
+              flex flex-col text-left gap-2 rounded-lg p-6 md:p-12 bg-blue-400
+            `}
+					>
+						<div
+							className={`
+                flex h-12 w-12 items-center justify-center rounded-lg
+                bg-blue-300 mb-4
+              `}
+						>
+							<PaintbrushVertical className="h-6 w-6 text-white" />
+						</div>
+						<strong className="text-heading-sm text-gray-100">
+							Personalize seu site
+						</strong>
+						<p className="text-body-sm text-gray-200">
+							Adicione sua logo, favicon, cores no seu catálogo e tenha tudo com
+							a sua cara.
+						</p>
+					</div>
+
+					{/* Segundo Card */}
+					<div
+						className={`
+              flex flex-col text-left gap-2 rounded-lg p-6 md:p-12 bg-cyan-300
+            `}
+					>
+						<div
+							className={`
+                flex h-12 w-12 items-center justify-center rounded-lg
+                bg-cyan-200 mb-4
+              `}
+						>
+							<Store className="h-6 w-6 text-white" />
+						</div>
+						<strong className="text-heading-sm text-gray-100">
+							Venda de qualquer loja
+						</strong>
+						<p className="text-body-sm text-gray-200">
+							Não importa a loja, o Site.Set permite que você insira qualquer
+							link de afiliado.
+						</p>
+					</div>
+
+					{/* Terceiro Card */}
+					<div
+						className={`
+              flex flex-col text-left gap-2 rounded-lg p-6 md:p-12 bg-blue-400
+            `}
+					>
+						<div
+							className={`
+                flex h-12 w-12 items-center justify-center rounded-lg
+                bg-blue-300 mb-4
+              `}
+						>
+							<HeartHandshake className="h-6 w-6 text-white" />
+						</div>
+						<strong className="text-heading-sm text-gray-100">
+							Receba suporte amigável
+						</strong>
+						<p className="text-body-sm text-gray-200">
+							Nossa equipe estará sempre pronta para te atender para ajudar no
+							que for preciso.
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
